a196f311bd3c56836501b6acdc515f012190a8fb
geoffrey woodcock <theatomproject@gmail.com>
2019-04-27 15:42:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(drivers, match) {
+  let newMatch = drivers.filter(function(matches) {
+    let check = match.toLowerCase();
+    if (matches === match) {
+        return matches === match;
+    }
+    else if (matches === check) {
+        return matches === check;
+    }
+  } );
+  return newMatch;
+}
+
+function matchName(drivers, match){
+  return drivers.filter(function(aa){
+    return aa.toLowerCase().indexOf(match.toLowerCase()) == 0;
+  })
+}
